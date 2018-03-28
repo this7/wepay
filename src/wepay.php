d34ceed9e5e80d3a86eae69a935a0a06a9d52d7d
qinuoyun <qinuoyun@qq.com>
2018-03-28 10:07:23 +0800

    no message

---
+++ src/wepay.php
@@ -0,0 +1,13 @@
+<?php
+namespace this7\wepay;
+/**
+ * This7 Frame
+ * @Author: else
+ * @Date:   2018-01-11 14:04:08
+ * @Last Modified by:   qinuoyun
+ * @Last Modified time: 2018-03-28 10:06:57
+ */
+
+class wepay {
+
+}
